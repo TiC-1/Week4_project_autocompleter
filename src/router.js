72b29c64b87b28dbbd32b5186f707937a11353c9
Claudio Scafesi <scafesi@gmail.com>
2018-02-28 10:35:19 +0100

    answer to your searches

--- src/router.js
+++ src/router.js
@@ -1,17 +1,13 @@
 var fs = require("fs");
 var querystring = require("querystring");
 var staticHandler = require("./handler.js");
-var create = require("./blogpost/create.js");
-var index = require("./blogpost/index.js");
 
 function router(request, response) {
   var endpoint = request.url;
   if (endpoint === "/") {
     handler.index(request, response);
-  } else if (endpoint === "/blogpost/create") {
-    create(request, response);
-  } else if (endpoint === "/blogposts") {
-    index(request, response);
+  } else if (endpoint === "/search") {
+  response.end(JSON.stringify(["a", "b", "c", "d", "f", "a", "b", "c", "d", "f"]));
   } else {
     handler.assets(request, response);
 
--- test/test-opentxt.js
+++ test/test-opentxt.js
@@ -1,16 +1,22 @@
 var test = require("tape");
 var tapSpec = require("tap-spec");
-var index = require("../src/index.js");
+var router = require("../src/router.js");
 var supertest = require("supertest");
 var fs = require("fs");
 
-// test("ciao .txt test", function (assert){
-// supertest(router)
-// .get("/")
-// .expect(200)
-// .end(function(error, response){
-//
-//   assert.ok(response.text.includes("A"),
-//    "we can open the .txt");
-//     assert.end();
-// });
+test("ciao .txt test", function (assert){
+supertest(router)
+.get("/search")
+.expect(200)
+.end(function(error, response){
+assert.equal(response.text, JSON.stringify(["a", "b", "c", "d", "f", "a", "b", "c", "d", "f"]), "risposta test ciaociao");
+assert.end();
+});
+});
+
+
+
+
+// assert.ok(response.text.includes("A"),
+//  "we can open the .txt");
+//   assert.end();
