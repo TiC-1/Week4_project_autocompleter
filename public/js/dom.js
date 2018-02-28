ae400895cbc9ff730f1a5489e584fa3a81c14230
Iannis Camillieri <ianniscamillieri@gmail.com>
2018-02-28 22:38:42 +0100

    little front-end js files improvement

--- public/js/dom.js
+++ public/js/dom.js
@@ -2,34 +2,37 @@
 var container = document.getElementById("acPreview");
 
 
-// Function ti track any change in the form text field in index.html
+// Function to track any change in the form text field in index.html
 function getInputValue() {
   var str = document.getElementById("input_box").value;
-  // Launch main function in logic.js with updateDOM as a callback
+  // Launch main (or first) function in logic.js with updateDOM as a callback
   getWordlist(updateDOM, str);
 }
 
 
 // General function to update DOM
+// (We keep it like this for potential further developments)
 function updateDOM(state) {
-    if (container) renderState(state);
+  if (container) renderState(state);
 }
 
 
 // General function to render the new state
-var renderState = function(state) {
+function renderState(state) {
+  // Create a new <ul></ul> with 'acList' class
   var wordUL_node = document.createElement('ul');
   wordUL_node.setAttribute('class', 'acList');
+  // Insert <li> calling createLI function
   state.forEach(function(item) {
-    wordUL_node.appendChild(createLi(item));
+    wordUL_node.appendChild(createLI(item));
   });
-  // Repplace old state with the new one
+  // Replace previous 'container' content (<ul>...</ul>)
   container.replaceChild(wordUL_node, container.firstChild);
 };
 
 
 // General function to create list items
-var createLi = function(words) {
+function createLI(words) {
   // Create a new <li></li>
   var wordLI_node = document.createElement("li");
   //Add some class to it to style it
