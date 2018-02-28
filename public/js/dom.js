4fb8b6405c1523327a80556f1c2b4ea164940847
Abrax <matte.work@live.it>
2018-02-28 14:33:04 +0100

    get the string from the server into the dom and renamed a typo in server.js

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,17 +1,24 @@
 var container = document.getElementById("acPreview");
 
-function updateDom(state){
-    console.log(state);
-}
+
+function updateDOM(state) {
+  // 1. crea un nodo con createSongNode
+  // 2. inserisce titolo e cover della canzone
+  var words = state;
+  console.log(words);
+  if (container) renderState(words);
+
+} //fine della funzione updateDOM
 
 function getInputValue(){
     var str = document.getElementById("input_box").value;
-     getWordlist(updateDom, str);   
+     getWordlist(updateDOM, str);   
 }
 
 
 var createLi = function(words) {
     console.log(words);
+ 
     var wordLi = document.createElement("li");
   
     //added some classes for the style in the DOM
@@ -29,18 +36,13 @@ var createLi = function(words) {
   
 
   var renderState = function(state) {
+    var words = state;
     var wordLiNode = document.createElement('ul');
-    state.forEach(function(s) {
-      wordLiNode.appendChild(createLi(s));
+    console.log("oggetto state:" + words);
+    console.log("oggetto state:" + state);
+    words.forEach(function(x) {
+      wordLiNode.appendChild(createLi(x));
     });
     container.replaceChild(wordLiNode, container.firstChild);
   };
   
-  function updateDOM() {
-    // 1. crea un nodo con createSongNode
-    // 2. inserisce titolo e cover della canzone
-    var words = testobj;
-    console.log(testobj);
-    if (container) renderState(words);
-  
-  } //fine della funzione updateDOM
--- public/js/logic.js
+++ public/js/logic.js
@@ -1,11 +1,12 @@
-var server = "localhost:3002"; //change in function of the server
+var server = "http://localhost:3006"; //change in function of the server
 
 function request(cb, url) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) { // Request is completed
         if (xhr.status === 200) { // Request succeeded
-          var responseObj = JSON.parse(xhr.responseText); // ok?
+          var responseObj = JSON.parse(xhr.responseText);
+          console.log("response=" + responseObj); // ok?
           cb(null, responseObj);
         } else { // Error in request
           var errorMessage = xhr.responseText;
