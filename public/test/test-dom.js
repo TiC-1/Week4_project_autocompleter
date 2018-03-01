079332e4bd9e1db5697435481bb1b72bd8edf703
Abrax <matte.work@live.it>
2018-03-01 23:50:10 +0100

    fixed first test and tryed a second

--- public/test/test-dom.js
+++ public/test/test-dom.js
@@ -3,8 +3,8 @@
     updateDOM(testobj);
   
     assert.equal(
-      document.querySelector("#acPreview").innerHTML,
-      "<ul><li class=\"li_item\">Aaron</li><li class=\"li_item\">Aaronic</li><li class=\"li_item\">Aaronical</li><li class=\"li_item\">Aaronite</li><li class=\"li_item\">Aaronitic</li><li class=\"li_item\">Aaru</li><li class=\"li_item\">Ab</li><li class=\"li_item\">aba</li><li class=\"li_item\">Ababdeh</li><li class=\"li_item\">Ababua</li></ul>", "there is not a ul!!!");
+    document.querySelector(".acList").textContent,
+      "AaronAaronicAaronicalAaroniteAaroniticAaruAbabaAbabdehAbabua", "there is not a ul!!!");
   
     assert.equal(
       document.querySelectorAll("#acPreview li").length,
@@ -12,5 +12,13 @@
       "Inserts 10 words");
   });
   
+  /*
+  test("renderState Test", function(assert) {
+    var obj = "AaronAaronicAaronicalAaroniteAaroniticAaruAbabaAbabdehAbabua";
+    var rs = renderState(obj);
   
-  
+   assert.ok(rs.querySelector("li").href.includes('["Aaron" , "Aaronic" , "Aaronical","Aaronite" , "Aaronitic" , "Aaru" ,"Ab" , "aba" , "Ababdeh" , "Ababua"];'), "Video node has YouTube link");
+    assert.ok(rs.querySelector(".acWords").lenght(10) , "Video node has picture");
+    assert.ok(renderaState.textContent.includes("Sunny"), "Video node title");
+  });
+  */
