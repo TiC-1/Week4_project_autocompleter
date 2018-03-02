0016d2daa13911432a8ea9b9b14a8ee35111da26
Claudio Scafesi <scafesi@gmail.com>
2018-03-02 10:38:00 +0100

    Update test-dom.js

--- public/test/test-dom.js
+++ public/test/test-dom.js
@@ -12,13 +12,3 @@
       "Inserts 10 words");
   });
   
-  /*
-  test("renderState Test", function(assert) {
-    var obj = "AaronAaronicAaronicalAaroniteAaroniticAaruAbabaAbabdehAbabua";
-    var rs = renderState(obj);
-  
-   assert.ok(rs.querySelector("li").href.includes('["Aaron" , "Aaronic" , "Aaronical","Aaronite" , "Aaronitic" , "Aaru" ,"Ab" , "aba" , "Ababdeh" , "Ababua"];'), "Video node has YouTube link");
-    assert.ok(rs.querySelector(".acWords").lenght(10) , "Video node has picture");
-    assert.ok(renderaState.textContent.includes("Sunny"), "Video node title");
-  });
-  */
